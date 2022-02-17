5ef22b49ee3f9e5659fe9e5c9afae72524fbe833
Yeray Li Loaiza <yeray@pop-os.localdomain>
2022-02-18 00:24:08 +0100

    trabajo

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
 </script>
 
 <template>
-  <div id="nav">
-      <img alt="Vue logo" src="/logo_black.png" />
+  <div class="nav">
+      <img alt="Vue logo" src="/logo_black.png" id="logo" />
       <div class="navbar">
         <router-link :to="{ name: 'Home' }">Home</router-link>
         <router-link :to="{ name: 'About' }">About</router-link>
@@ -13,18 +13,33 @@
 </template>
 
 <style>
+@import "./assets/base.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.nav {
+  width: auto;
+  padding: 1rem 2rem;
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+  background-color: #f1f1f1;
+}
+
+#logo {
+  width: 20rem;
+  max-height: auto;
 }
 
 .navbar {
   display: flex;
   justify-content: center;
+  align-items: baseline;
   gap: .5rem;
 }
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,30 @@
 <script setup>
-    import HelloWorld from '../components/HelloWorld.vue';
+    import PokemonCard from '../components/PokemonCard.vue';
+
+    const getPokemons = async () => {
+        const res = await fetch('https://pokeapi.co/api/v2/pokemon')
+        const data = await res.json();
+        return data
+
+    }
+        
+    const pokemons = getPokemons
+
+    console.log(pokemons);
+
+
 </script>
 
 <template>
-    <div>
-        <h1>Hello from Home.vue</h1>
-        <HelloWorld></HelloWorld>
+    <div class="card-container">
+        <h1>cartas</h1>
+        <PokemonCard></PokemonCard>
     </div>
 </template>
+
+<style>
+    .card-container {
+        display: flex;
+        width: auto;
+    }
+</style>
