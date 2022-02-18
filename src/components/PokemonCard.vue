efefd40dd89280821a4b564b7d79261d4ca09b1a
Yeray <yerayliloaiza@gmail.com>
2022-02-18 13:59:29 +0100

    carta de pokemon

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -1,7 +1,29 @@
 <script setup>
+    const props = defineProps(['pokemon']);
+    const pokemon = props.pokemon;
 
+    fetch(pokemon.url)
+        .then( (res) => res.json() )
+        .then( (json) => (pokemon.detalle = json) )
+        .catch( (error) => (console.error(error.message)))
 </script>
 
 <template>
-    
+    <div class="card">
+        <img :src="pokemon.detalle.sprites.front_default" :alt="'foto de '+pokemon.name">
+        <h1>{{pokemon.name}}</h1>
+        <!-- <a :href="pokemon.url">link</a> -->
+    </div>
 </template>
+
+<style>
+    .card{
+        border: solid 2px #dddddd;
+        width: 15rem;
+    }
+
+    .card img {
+        width: 100%;
+        max-height: auto;
+    }
+</style>
