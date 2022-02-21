3e79018218262a89f0a0a6a16908db10a28a0dd4
Yeray Li Loaiza <yerayliloaiza@gmail.com>
2022-02-22 00:01:44 +0100

    intento de asincrono

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,57 @@
 <script setup>
-    import { reactive } from 'vue';
-    import PokemonCard from '../components/PokemonCard.vue';
+import { defineAsyncComponent } from 'vue';
+import Loading from '../components/Loading.vue'
 
-    const state = reactive({
-        pokemons:[]
+const PokemonCard = defineAsyncComponent({
+    loader: () => import("../components/PokemonCard.vue"),
+    loadingComponent: Loading,
+    // mientras estan cargando las tarjetas carga el componente Loading
+    delay: 200,
+    suspensible: false
+})
+
+// const pokemons = fetch('https://pokeapi.co/api/v2/pokemon?offset=0')
+//     .then((response) => response.json())
+//     .then((data) => {
+//         data.results.forEach(pokemon => {
+//             pokemon.id = pokemon.url.split('/').slice(-2, -1).pop()
+//             delete pokemon.url
+//             // url ahora es el id y pusheamos el objeto pokemon en el array
+//         });
+//         return data.results
+//     })
+//     .catch((error) => (console.error(error.message)))
+const pokemons = async() => {
+    return fetch('https://pokeapi.co/api/v2/pokemon?offset=0')
+    .then((response) => response.json())
+    .then((data) => {
+        data.results.forEach(pokemon => {
+            pokemon.id = pokemon.url.split('/').slice(-2, -1).pop()
+            delete pokemon.url
+            // url ahora es el id y pusheamos el objeto pokemon en el array
+        });
+        return data.results
     })
-
-    fetch('https://pokeapi.co/api/v2/pokemon?offset=0')
-        .then( (response) => response.json() )
-        .then( (data) => {
-            data.results.forEach(pokemon => {
-                pokemon.id = pokemon.url.split('/').slice(-2, -1).pop()
-                delete pokemon.url
-                state.pokemons.push(pokemon)
-                // url ahora es el id y pusheamos el objeto pokemon en el array
-            });
-        })
-        .catch( (error) => (console.error(error.message)))
+    .catch((error) => (console.error(error.message)))
+    }
 </script>
 
 <template>
+    <pre>{{pokemons }}</pre>
     <div class="card-container">
-        <PokemonCard v-for="pokemon in state.pokemons" :id="pokemon.id" :key="pokemon.id"></PokemonCard>
+        <TransitionGroup appear>
+            <PokemonCard v-for="pokemon in pokemons" :id="pokemon.id" :key="pokemon.id"></PokemonCard>
+        </TransitionGroup>
     </div>
 </template>
 
 <style>
-    .card-container {
-        display: flex;
-        flex-flow: wrap row;
-        justify-content: center;
-        width: auto;
-        gap: 1rem;
-    }
+.card-container {
+    display: flex;
+    flex-flow: wrap row;
+    justify-content: center;
+    width: auto;
+    gap: 1rem;
+    margin: 1rem 0;
+}
 </style>
