fc35d5889d240e247fe0c09b41dd4508aefcca4d
Yeray <yerayliloaiza@gmail.com>
2022-02-21 13:28:53 +0100

    detalle

--- src/views/Detalle.vue
+++ src/views/Detalle.vue
@@ -1,10 +1,23 @@
 <script setup>
+    import { reactive } from 'vue';
+    
     const props = defineProps(['id']);
     const id = props.id;
+
+    const state = reactive({
+        detalle:[]
+    });
+
+    fetch("https://pokeapi.co/api/v2/pokemon/"+id)
+        .then( (response) => response.json() )
+        .then( (data) => (state.detalle=data) )
+        .catch( (error) => (console.error(error.message)))
 </script>
 
 <template>
     <div>
         <pre>{{id}}</pre>
+        <img :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/'+id+'.png'" :alt="'foto de '+state.detalle.name">
+
     </div>
 </template>
