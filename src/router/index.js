b0e97101ca6b46ec01a6d633acca310f26d0c471
Yeray <yerayliloaiza@gmail.com>
2022-02-21 12:55:33 +0100

    ruta para detalle

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from "../views/Home.vue";
+import Detalle from "../views/Detalle.vue"
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/:id",
+    name: "Detalle",
+    component: Detalle
   },
 ];
 
---
+++ src/views/Detalle.vue
@@ -0,0 +1,10 @@
+<script setup>
+    const props = defineProps(['id']);
+    const id = props.id;
+</script>
+
+<template>
+    <div>
+        <pre>{{id}}</pre>
+    </div>
+</template>
