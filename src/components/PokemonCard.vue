d42c64010cd754266888626310a513a993b0091a
JonGonzalezPallares <jongonzalezpallares0@gmail.com>
2022-02-22 10:44:51 +0100

    los tipos e imagenes cargadas

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -1,12 +1,16 @@
 <script setup>
     const props = defineProps(['id']);
     const id = props.id;
+    const tipos=[];
 
     const detalle = await fetch("https://pokeapi.co/api/v2/pokemon/"+id)
         .then( (response) => response.json() )
         .then( (data) => {
             // Poner la primera letra del nombre en mayus
             data.name = data.name.charAt(0).toUpperCase() + data.name.slice(1)
+            for(let paso=0; paso<data.types.length; paso++){
+                tipos.push("./Tipos/"+data.types[paso].type.name+".svg")
+            }
             return data
         })
         .catch( (error) => (console.error(error.message)))
@@ -16,9 +20,9 @@
     <div class="card">
         <router-link :to="{name: 'Detalle', params: {id: id}}">
             <h1>{{detalle.name}}</h1>
-            <img :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/'+id+'.png'" :alt="'foto de '+detalle.name">
+            <img class="pokemon" :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/'+id+'.png'" :alt="'foto de '+detalle.name">
             <div class="cajatipos" >
-                <img class="tipos" v-for="tipo in detalle.types" :key="tipo.slot" :src="tipo">
+                <img class="tipos" v-for="tipo in tipos" :key="tipo.slot" :src="tipo">
             </div>
         </router-link>
     </div>
@@ -33,10 +37,6 @@
         border: solid 2px #dddddd;
         width: 20rem;
         transition: 0.4s;
-    }
-    .card img {
-        width: 100%;
-        max-height: auto;
     }
 
     .card:hover{
