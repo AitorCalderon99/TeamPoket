62a4d9c336197decc11b4a74dc23c6255dc090c5
JonGonzalezPallares <jongonzalezpallares0@gmail.com>
2022-02-22 09:07:51 +0100

    lo de los tipos

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -23,7 +23,7 @@
         <router-link :to="{name: 'Detalle', params: {id: id}}">
             <h1>{{state.detalle.name}}</h1>
             <img :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/'+id+'.png'" :alt="'foto de '+state.detalle.name">
-            <pre>{{ state.detalle.types }}</pre>
+            <pre>{{ state.detalle.types[0].type.name }}</pre>
         </router-link>
     </div>
 </template>
