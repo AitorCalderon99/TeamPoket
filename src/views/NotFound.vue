76ef4b0a705759b6e3f717cb39293a89b1f3387b
Yeray <yerayliloaiza@gmail.com>
2022-02-22 12:36:31 +0100

    404 not found

---
+++ src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <img src="/question.gif" alt="Error">
+        <h1>UPS! Ha habido un error</h1>
+    </div>
+</template>
+
+<style scoped>
+    div {
+        display: flex;
+        flex-flow: column nowrap;
+        justify-content: center;
+    }
+
+    img {
+        margin: auto;
+        width: 25%;
+    }
+</style>
