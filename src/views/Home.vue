f04940b053052b20b1d993b3372b0db0c8f6bdd3
AitorCalderon99 <90387604+AitorCalderon99@users.noreply.github.com>
2022-02-22 14:00:43 +0100

    Lazy loading pokemons in home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,88 @@
 <script setup>
-import { defineAsyncComponent } from 'vue';
+import {defineAsyncComponent, reactive} from 'vue';
 import Loading from '../components/Loading.vue'
+import {onMounted} from "vue";
+import axios from "axios";
 
-const PokemonCard = defineAsyncComponent({
-    loader: () => import("../components/PokemonCard.vue"),
-    loadingComponent: Loading,
-    // mientras estan cargando las tarjetas carga el componente Loading
-    delay: 200,
-    suspensible: false
+onMounted(() => {
+  loadFirstPokemons();
+  getNextUser();
 })
 
-const pokemons = await fetch('https://pokeapi.co/api/v2/pokemon')
+let url = 'https://pokeapi.co/api/v2/pokemon?offset=';
+let numb = 0;
+let allPokemons = reactive([]);
+
+function loadFirstPokemons() {
+  axios.get(url + numb).then(response => {
+    response.data.results.forEach(pokemon => {
+      pokemon.id = pokemon.url.split('/').slice(-2, -1).pop()
+      delete pokemon.url
+      // url ahora es el id y pusheamos el objeto pokemon en el array
+      allPokemons.push(pokemon);
+    })
+    numb += 20;
+  })
+}
+
+
+function getNextUser() {
+  window.onscroll = () => {
+    let bottomOfWindow = document.documentElement.scrollHeight - document.documentElement.scrollTop <= document.documentElement.clientHeight;
+    console.log(bottomOfWindow);
+    if (bottomOfWindow) {
+      axios.get(url + numb).then(response => {
+        response.data.results.forEach(pokemon => {
+          pokemon.id = pokemon.url.split('/').slice(-2, -1).pop()
+          delete pokemon.url
+          // url ahora es el id y pusheamos el objeto pokemon en el array
+          allPokemons.push(pokemon);
+        })
+        numb += 20;
+      })
+      console.log(allPokemons);
+    }
+
+  }
+}
+
+const PokemonCard = defineAsyncComponent({
+  loader: () => import("../components/PokemonCard.vue"),
+  loadingComponent: Loading,
+  // mientras estan cargando las tarjetas carga el componente Loading
+  delay: 200,
+  suspensible: false
+})
+
+/*const pokemons = await fetch('https://pokeapi.co/api/v2/pokemon')
     .then((response) => response.json())
     .then((data) => {
-        data.results.forEach(pokemon => {
-            pokemon.id = pokemon.url.split('/').slice(-2, -1).pop()
-            delete pokemon.url
-            // url ahora es el id y pusheamos el objeto pokemon en el array
-        })
-        return data.results;
+      data.results.forEach(pokemon => {
+        pokemon.id = pokemon.url.split('/').slice(-2, -1).pop()
+        delete pokemon.url
+        // url ahora es el id y pusheamos el objeto pokemon en el array
+      })
+      return data.results;
     })
-    .catch((error) => (console.error(error.message)))
+    .catch((error) => (console.error(error.message)))*/
 </script>
 
 <template>
-    <div class="card-container">
-        <TransitionGroup appear>
-            <PokemonCard v-for="pokemon in pokemons" :id="pokemon.id" :key="pokemon.id"></PokemonCard>
-        </TransitionGroup>
-    </div>
+  <div class="card-container">
+    <TransitionGroup appear>
+      <PokemonCard v-for="pokemon in allPokemons" :id="pokemon.id" :key="pokemon.id"></PokemonCard>
+    </TransitionGroup>
+  </div>
 </template>
 
 <style scoped>
 .card-container {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
-    justify-content: center;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  justify-content: center;
 
-    width: 100%;
-    gap: 1rem;
-    margin: 1rem 0;
+  width: 100%;
+  gap: 1rem;
+  margin: 1rem 0;
 }
 </style>
